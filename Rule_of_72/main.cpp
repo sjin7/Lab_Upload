4d38e0e10048b5e4166e2b100b6e881b35bf3479
sjin7 <jsbb_7@hotmail.com>
2014-07-02 13:46:49 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Rule_of_72/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on July 2, 2014, 1:25 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    //Input
+    //Declare Variables
+    int N; //number of years
+    
+    //Output
+    //Declare Variables
+    float i; //interest rate
+    float number; // number should be close to 2
+       
+    //Prompt the User
+    cout << "How long is the term(in years)?" << endl;
+    cin >> N;
+       
+    //Calculation
+    i = 72/N/100;
+    number = pow(1 + i, N);
+    cout << number;
+    
+    return 0;
+}
+
